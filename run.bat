a315316613c48442db8c8b934b62cc3890e79aa1
Hesamodin-shafie <hesamodinshafiee@gmail.com>
2025-01-10 17:47:35 +0330

    Check won

--- run.bat
+++ run.bat
@@ -4,9 +4,12 @@ cd /d "%~dp0"
 
 rem Run each Python file in separate command windows
 start "Server" cmd /k python server.py
+timeout 1
 start "R3" cmd /k python r3.py
+timeout 1
 start "R2" cmd /k python r2.py
+timeout 1
 start "R1" cmd /k python r1.py
-
+timeout 1
 @REM rem Optional: Pause the batch script if needed
 @REM pause
